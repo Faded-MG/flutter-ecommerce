9b6096f8248611b4bb86184659c47929c4bc87ad
FADED <mahletgetinet17@gmail.com>
2026-08-08 01:41:53 +0300

    feat: add UserModel class for user data representation and JSON parsing

---
+++ lib/features/auth/model/user_model.dart
@@ -0,0 +1,35 @@
+class UserModel {
+  final int id;
+  final String email;
+  final String username;
+  final String firstname;
+  final String lastname;
+  final String phone;
+  final String city;
+  final String street;
+
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.username,
+    required this.firstname,
+    required this.lastname,
+    required this.phone,
+    required this.city,
+    required this.street,
+  });
+
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      email: json['email'],
+      username: json['username'],
+      firstname: json['name']['firstname'],
+      lastname: json['name']['lastname'],
+      phone: json['phone'],
+      city: json['address']['city'],
+      street: json['address']['street'],
+    );
+  }
+}
