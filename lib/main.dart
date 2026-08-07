ebf859ba8274cfbc595e2ae212db0df28d968265
FADED <mahletgetinet17@gmail.com>
2026-08-07 22:37:06 +0300

    fixed the main.dart to call the login page first

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'features/home/presentation/home_screen.dart';
+import 'features/auth/presentation/auth_gate.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'E-Commerce App',
-      home: const HomeScreen(),
+      home: const AuthGate(),
     );
   }
 }
